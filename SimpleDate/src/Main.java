eb0f9aa195087763dfaee91381e0a5e62ca95970
KamilaSouza <kamisouzas@gmail.com>
2021-07-15 12:30:23 -0300

    Date

---
+++ SimpleDate/src/Main.java
@@ -0,0 +1,36 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Main {
+
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat date1 = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat date2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        SimpleDateFormat date3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        date3.setTimeZone(TimeZone.getTimeZone("GMT")); // to set to GMT time
+
+        Date d1 = date1.parse("15/07/2021");
+        Date d2 = date2.parse("15/07/2021 11:41:11");
+        Date d3 = new Date();
+        Date d4 = new Date(System.currentTimeMillis());
+        Date d5 = new Date(0L);
+        Date d6 = new Date(1000L * 60 * 60 * 5);  //1000 milliseconds, 60 seconds, 60 minutes, 5 hours
+        Date d7 = Date.from(Instant.parse("2021-07-15T12:05:01Z"));
+
+        System.out.println(date2.format(d1));
+        System.out.println(date3.format(d1));
+        System.out.println();
+        System.out.println(date2.format(d3));
+        System.out.println(date3.format(d3));
+        System.out.println();
+        System.out.println(d3);
+        System.out.println(d4);
+        System.out.println(d5);
+        System.out.println(d6);
+        System.out.println("ISO " + d7);
+    }
+}
