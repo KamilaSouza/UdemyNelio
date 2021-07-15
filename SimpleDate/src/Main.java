ae78e09a716cb9f3f8bd97ebba2c6b452545ac17
KamilaSouza <kamisouzas@gmail.com>
2021-07-15 12:53:37 -0300

    Date and Calendar

--- SimpleDate/src/Main.java
+++ SimpleDate/src/Main.java
@@ -1,6 +1,7 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -21,16 +22,27 @@ public class Main {
         Date d6 = new Date(1000L * 60 * 60 * 5);  //1000 milliseconds, 60 seconds, 60 minutes, 5 hours
         Date d7 = Date.from(Instant.parse("2021-07-15T12:05:01Z"));
 
-        System.out.println(date2.format(d1));
-        System.out.println(date3.format(d1));
-        System.out.println();
-        System.out.println(date2.format(d3));
-        System.out.println(date3.format(d3));
-        System.out.println();
-        System.out.println(d3);
-        System.out.println(d4);
-        System.out.println(d5);
-        System.out.println(d6);
-        System.out.println("ISO " + d7);
+        System.out.println(d7);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(d7);
+        calendar.add(Calendar.HOUR_OF_DAY, 4);
+        d7 = calendar.getTime();
+        int minutes = calendar.get(calendar.MINUTE);
+        int month = 1 + calendar.get(calendar.MONTH); // calendar starts at 0 (january)
+        System.out.println(minutes);
+        System.out.println(month);
+
+        System.out.println(date2.format(d7));
+//        System.out.println(date3.format(d1));
+//        System.out.println();
+//        System.out.println(date2.format(d3));
+//        System.out.println(date3.format(d3));
+//        System.out.println();
+//        System.out.println(d3);
+//        System.out.println(d4);
+//        System.out.println(d5);
+//        System.out.println(d6);
+//        System.out.println("ISO " + d7);
     }
 }
