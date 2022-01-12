14b627753f5d8255608695471bfef0f42e4a5c52
KamilaSouza <kamisouzas@gmail.com>
2022-01-12 11:13:33 -0300

    Abstract class

--- Inheritance/src/Main.java
+++ Inheritance/src/Main.java
@@ -4,11 +4,13 @@ import entities.SavingsAccount;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US); // dot
         List<Account> list = new ArrayList<>();
 
         list.add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
