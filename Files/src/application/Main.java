22255cb8bb9789be0eff26dad3243c5b325dd328
KamilaSouza <kamisouzas@gmail.com>
2022-02-08 10:47:53 -0300

    Files with scanner

---
+++ Files/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // \ before special characters (\)
+        File file = new File("c:\\temp\\in.txt");
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally { // close scanner in block finally is a good practise
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
+}
